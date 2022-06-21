d11047e5b924294cbd51909f243cb43b9790451c
Schlabbster <christoph.lubkowitz@gmx.de>
2022-06-21 10:26:54 +0200

    Zwischenstand, CaretakerDAO

--- src/main/java/datastorage/CaretakerDAO.java
+++ src/main/java/datastorage/CaretakerDAO.java
@@ -1,43 +1,60 @@
 package datastorage;
 
+import model.Caretaker;
+import utils.DateConverter;
+
+import javax.swing.text.Caret;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class CaretakerDAO<T> extends DAOimp<T> {
+public class CaretakerDAO<T> extends DAOimp<Caretaker> {
     public CaretakerDAO(Connection conn) {
         super(conn);
-    }
+        }
 
     @Override
-    protected String getCreateStatementString(T t) {
-        return null;
+    protected String getCreateStatementString(Caretaker caretaker) {
+        return String.format("INSERT INTO caretaker (cid, firstname, surname, phonenumber) VALUES ('%s', '%s', '%s', '%s')",
+                caretaker.getCid(), caretaker.getFirstName(), caretaker.getSurname(), caretaker.getPhoneNumber());
     }
 
     @Override
     protected String getReadByIDStatementString(long key) {
-        return null;
+        return String.format("SELECT * FROM caretaker WHERE cid = %d", key);
     }
 
     @Override
-    protected T getInstanceFromResultSet(ResultSet set) throws SQLException {
-        return null;
+    protected Caretaker getInstanceFromResultSet(ResultSet result) throws SQLException {
+        Caretaker c = null;
+        c = new Caretaker(result.getString(1), result.getString(2),
+                result.getInt(3), result.getLong(4));
+        return c;
     }
 
     @Override
     protected String getReadAllStatementString() {
-        return null;
+        return "SELECT * FROM caretaker";
     }
 
     @Override
-    protected ArrayList<T> getListFromResultSet(ResultSet set) throws SQLException {
-        return null;
+    protected ArrayList<Caretaker> getListFromResultSet(ResultSet result) throws SQLException {
+        ArrayList<Caretaker> list = new ArrayList<Caretaker>();
+        Caretaker c = null;
+        while (result.next()) {
+            c = new Caretaker(result.getString(1), result.getString(2),
+                    result.getInt(3), result.getLong(4));
+            list.add(c);
+        }
+        return list;
     }
 
     @Override
-    protected String getUpdateStatementString(T t) {
-        return null;
+    protected String getUpdateStatementString(Caretaker caretaker) {
+        return String.format("UPDATE caretaker SET firstname = '%s', surname = '%s', phonenumber = '%s', " +
+                "WHERE cid = %d", caretaker.getFirstName(), caretaker.getSurname(), caretaker.getCid(), caretaker.getPhoneNumber());
     }
 
     @Override
@@ -45,3 +62,7 @@ public class CaretakerDAO<T> extends DAOimp<T> {
         return null;
     }
 }
+
+
+
+
